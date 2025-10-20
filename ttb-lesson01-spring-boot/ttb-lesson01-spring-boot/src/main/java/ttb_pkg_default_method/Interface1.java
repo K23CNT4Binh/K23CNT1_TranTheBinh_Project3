ba61705548f911f5a7529ea66a56291903d3fd9e
unknown <binhtranthe2005@gmail.com>
2025-10-20 09:39:37 +0700

    ttb-lesson01-spring-boot

---
+++ ttb-lesson01-spring-boot/ttb-lesson01-spring-boot/src/main/java/ttb_pkg_default_method/Interface1.java
@@ -0,0 +1,7 @@
+package ttb_pkg_default_method;
+
+interface Interface1 {
+    default void method1() {
+        System.out.println("Interface1.method1");
+    }
+}
---
+++ ttb-lesson01-spring-boot/ttb-lesson01-spring-boot/src/main/java/ttb_pkg_default_method/Shape.java
@@ -0,0 +1,10 @@
+package ttb_pkg_default_method;
+
+public interface Shape {
+    void draw();
+
+    default void setColor(String color) {
+        System.out.println("Vẽ hình với màu: " + color);
+    }
+}
+
